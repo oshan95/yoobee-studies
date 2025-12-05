f7272a7ad5e800d83c1ad123e16b50f0237a480a
Oshan Mendis <pobudumendis@gmail.com>
2025-12-06 09:21:35 +1300

    Updated activity 3

--- MSE800 - Professional Software Development/week3/ReadFile.py
+++ MSE800 - Professional Software Development/week3/ReadFile.py
@@ -19,6 +19,12 @@ class FileReader:
         data.close()
         return count
 
+    def add_content(self, content):
+        with open(self.file_path, "a") as data:
+            data.write(content)
+            data.write("\n")
+
+
 
 if __name__ == "__main__":
 
@@ -26,3 +32,5 @@ if __name__ == "__main__":
     file_reader.read_file()
     total_star_count = file_reader.find_char_count("*")
     print("Number of '*' found:", total_star_count)
+
+    file_reader.add_content("End of File");
